94f144a7f9f22a7f1a541070f0d9f9bf6ce21b8e
gschamika <97242725+gschamika@users.noreply.github.com>
2024-01-21 19:01:50 +0530

    commit 1

---
+++ src/layout/Navbar.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Full Stack Application
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </nav>
+    </div>
+  );
+}
